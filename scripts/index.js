60729e0081e25716e022f8bb5c6502b65d0f2e3a
Tomiwa Orimoloye <gabeorimoloye@gmail.com>
2022-02-05 00:12:56 +0100

    mobile version 1.1 with interactive mobile nav

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,10 @@
-console.log("Script is connected!");
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector("#navlinks");
+
+burger.addEventListener("click", toggleBurger);
+
+let visible = false;
+function toggleBurger() {
+	visible = !visible;
+	navLinks.style.display = visible ? "block" : "none";
+}
